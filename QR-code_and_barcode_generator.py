6359076270949731d1cdab25ccfd40ec084369fa
Rácz Gergő <raczgprogramming@gmail.com>
2024-08-17 01:05:04 +0200

    V1.0.1.0
    Possibility to specify the file save location added

--- QR-code_and_barcode_generator.py
+++ QR-code_and_barcode_generator.py
@@ -1,18 +1,30 @@
 import qrcode
 import tkinter as tk
-from datetime import datetime
+from tkinter import filedialog
 import barcode
 from barcode.writer import ImageWriter
 import qrcode.constants
 
+def filesave():
+    file_path = filedialog.asksaveasfilename(
+        defaultextension="png",
+        filetypes=[("PNG files", "*.png"), ("All files", "*.*")]
+    )
+    
+    if not file_path:
+        return None
+    
+    return file_path
 
 def generating_barcode():
     try:
-        now_time1 = datetime.now()
         bdata = input_value.get()
         bcode = barcode.get("code128", bdata, writer=ImageWriter())
-        time1 = now_time1.strftime("%Y%m%d%H%M%S")
-        filename = bcode.save(f"barcode{time1}")
+        f_path = filesave()
+        if f_path is None:
+            return None
+        f_path = f_path.rsplit('.',1)[0]
+        filename = bcode.save(f_path)
         return filename
     except barcode.errors.IllegalCharacterError as ICE:
         message_label.config(text="Nem engedélyezett karaktert használtál!", bg="red")
@@ -28,16 +40,17 @@ def generating_barcode():
 
 def generating_qrcode():
     try:
-        now_time2 = datetime.now()
         qdata = input_value.get()
         qcode = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qcode.add_data(qdata)
         qcode.make(fit=True)
         img = qcode.make_image(fill_color="black", back_color="white")
-        time2 = now_time2.strftime("%Y%m%d%H%M%S")
-        filename = f"qrcode{time2}.png"
-        img.save(filename)
-        return filename
+        filename = filesave()
+        if filename is None:
+            return None
+        else:
+            img.save(filename)
+            return filename
     except Exception as EX:
         message_label.config(text="Ismeretlen hiba történt!", bg="red")
         mainwindow.after(5000,reset_message)
